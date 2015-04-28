70145c5c6f4f363d2a8f53d6fa59ba8443aec5eb
Phil Nova <pnova8@gmail.com>
2015-04-27 23:44:34 -0700

    Test code throwing AssertionError, need to figure out why

--- oligo_extend.py
+++ oligo_extend.py
@@ -5,6 +5,16 @@ def create_string(length, dash_prob = 0.1):
 	modified = [i if random.random() > dash_prob else '-' for i in initial]
 	return ''.join(modified)
 
+def len_without_dashes(string):
+	counter = 0
+	#dash_counter = 0
+	for char in string:
+		if not char == "-":
+			counter += 1
+		#else:
+			#dash_counter += 1
+	return counter #, dash_counter
+
 def check_repeat(string, repeat_tolerance = 8, check_from = "left"):
 	if check_from == "right":
 		string = "".join([i for i in reversed(string)])
@@ -39,29 +49,34 @@ class Oligo(object):
 		self.gaps_left, self.gaps_right = 0, 0
 
 		#march outward on both sides
-		while self.length + self.extension_left + self.extension_right < target_length:
+		while len_without_dashes(self.left_string + self.string + self.right_string) < target_length:
 			if extend_left:
 				current_char = self.longer_string[self.substring_start - 1]
+				
 				if not current_char in escape_chars.keys():
-					self.left_string = current_char + self.left_string
+					
 					self.extension_left += 1
+					
 				else:
-					self.gaps_left += escape_chars[current_char]
+					self.gaps_left += 1 #escape_chars[current_char]
+				
+				self.left_string = current_char + self.left_string
 				self.substring_start -= 1
 
-			if self.length + self.extension_left + self.extension_right == target_length:
+			if len_without_dashes(self.left_string + self.string + self.right_string) == target_length:
 				break
 
 			if extend_right:
-				current_char = self.longer_string[self.substring_end + 1]
+				current_char = self.longer_string[self.substring_end]
+
 				if not current_char in escape_chars.keys():
-					self.right_string += current_char
+					
 					self.extension_right += 1
 				else:
-					self.gaps_right += escape_chars[current_char]
+					self.gaps_right += 1 #escape_chars[current_char]
+
+				self.right_string += current_char
 				self.substring_end += 1
-
-
 
 			#check whether repeat condition is violated
 			if extend_left:
@@ -74,7 +89,7 @@ class Oligo(object):
 					extend_left = False
 
 			if extend_right:
-				if self.substring_end == len(self.longer_string) - 2:
+				if self.substring_end == len(self.longer_string) - 1:
 					extend_right = False
 
 				if self.gaps_left <= gap_tolerance:
@@ -82,7 +97,14 @@ class Oligo(object):
 				else:
 					extend_right = False
 
+			if not extend_left and not extend_right:
+				print "No further extension possible"
+				break
+
 		self.extended_string = self.left_string + self.string + self.right_string
+
+		print len(self.extended_string), len_without_dashes(self.extended_string)
+
 		self.extend_left, self.extend_right = extend_left, extend_right
 	
 	def further_extend(self, amt_left=0, amt_right=0):
@@ -115,32 +137,32 @@ class OligoPair(object):
 		self.oligo2 = oligo2
 
 	def paired_extend(self, target_length):
-		oligo1.extend(target_length)
-		oligo2.extend(target_length)
+		self.oligo1.extend(target_length)
+		self.oligo2.extend(target_length)
 
-		if not len(oligo1.extended_string) == len(oligo2.extended_string):
+		if not len(self.oligo1.extended_string) == len(self.oligo2.extended_string):
 
 			#establish which is longer
-			diff = len(oligo1.extended_string) - len(oligo2.extended_string)
+			diff = len(self.oligo1.extended_string) - len(self.oligo2.extended_string)
 
 			#need to extend the shorter string on left and right
 			#take into account the number of gaps on each side, as well as whether we are allowed to keep extending to given side
 
-			diff_left = oligo1.extension_left - oligo2.extension_left
-			diff_right = oligo1.extension_right - oligo2.extension_right
+			diff_left = self.oligo1.extension_left - self.oligo2.extension_left
+			diff_right = self.oligo1.extension_right - self.oligo2.extension_right
 
-			assert (diff_left and diff_right > 0) or (diff_left and diff_right < 0)
-			assert diff == (oligo1.gaps_left - oligo2.gaps_left) + (oligo1.gaps_right - oligo2.gaps_right)
+			assert (diff_left > 0 and diff_right > 0) or (diff_left < 0 and diff_right < 0)
+			#assert diff == (self.oligo1.gaps_left - self.oligo2.gaps_left) + (self.oligo1.gaps_right - self.oligo2.gaps_right)
 
 			if diff > 0:
 
 				# oligo1 longer
-				oligo2.further_extend(diff_left, diff_right)
+				self.oligo2.further_extend(diff_left, diff_right)
 			else:
 				#oligo2 longer
-				oligo1.further_extend(-diff_left, -diff_right)
+				self.oligo1.further_extend(-diff_left, -diff_right)
 
-		return oligo1.extended_string, oligo2.extended_string
+		return self.oligo1.extended_string, self.oligo2.extended_string
 
 
 def file_parse(filename):
@@ -150,20 +172,26 @@ def main(input_filename, output_filename):
 	pass
 
 if __name__ == "__main__":
-	long_string = create_string(300)
-	short_string = long_string[100:151]
-	print long_string
-	print short_string
-	o = Oligo(short_string, long_string)
-	o.extend()
-	print len(o.extended_string)
-	print o.extension_left, o.extension_right
-	print o.gaps_left, o.gaps_right
-	o.further_extend(2,2)
-	print len(o.extended_string)
-	INPUT_FILENAME = ""
-	OUTPUT_FILENAME = ""
-	main(INPUT_FILENAME, OUTPUT_FILENAME)
+	long_string1, long_string2 = create_string(400), create_string(400)
+	short_string1, short_string2 = long_string1[100:250], long_string2[100:250]
+
+	o1 = Oligo(short_string1, long_string1)
+	o2 = Oligo(short_string2, long_string2)
+	
+	op = OligoPair(o1, o2)
+	op.paired_extend(171)
+
+	#print op.oligo1.extended_string
+	#print op.oligo2.extended_string
+
+	#print op.oligo1.extended_string
+	#print len(op.oligo1.extended_string) - op.oligo1.gaps_left - op.oligo1.gaps_right
+	#print op.oligo1.gaps_left, op.oligo1.gaps_right
+	print len_without_dashes(op.oligo1.extended_string), len_without_dashes(op.oligo2.extended_string)
+	print op.oligo1.extend_left, op.oligo1.extend_right
+
+	print len(op.oligo1.extended_string), len(op.oligo2.extended_string)
+
 
 
 
